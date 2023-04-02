4b2e5d53e4cbf3189d3d5871cdbf5a3a9e44e151
Mudassir Ishfaq <mudassirishfaq900@gmail.com>
2023-04-03 00:33:34 +0500

    Services section completed

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ let menuIcon = document.querySelector(".menu__icon");
 menuIcon.addEventListener("click", ()=>{
     menuIcon.classList.toggle("move")
 })
+
+// services swiper
+
+var swiper = new Swiper(".swiper__content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
